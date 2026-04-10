dc52216f6338aadf577db8761260bc7f1e59247e
ninjabrindesti <ninjabrindesti@gmail.com>
2026-04-10 13:09:06 -0300

    main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/generate")
+def generate_proposal(data: dict):
+    return {
+        "status": "success",
+        "message": "Payload recebido com sucesso",
+        "data_received": data
+    }
